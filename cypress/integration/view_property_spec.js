8c689b713333651f9534b107c0a5e4d6dcd41b03
Chris Russell <chris.john.russell@gmail.com>
2018-06-19 15:47:43 +0100

    add integration test for page content

--- cypress/integration/view_property_spec.js
+++ cypress/integration/view_property_spec.js
@@ -2,16 +2,21 @@ describe('Viewing a listing', function() {
 
   it('checks for "room with a pool content"', function() {
      cy.visit('http://127.0.0.1:8080/')
+     cy.click(Submit)
      cy.contains('Room with Pool!')
      })
 
-  it('clicks on the contact email link', function() {
+  it('checks for "description content"', function() {
+    cy.visit('http://127.0.0.1:8080/')
+    cy.contains('Room Description: Awesome poolside room with plenty of sun. Space for 2 capybaras. Lots of great night life and black our shades so you can sleep peacefully during the day. Grass, fruits and soft tree bark available for breakfast.')
+  })
+
+
+  xit('clicks on the contact email link', function() {
       cy.contains('mrcapybara@gmail.com').click()
      })
 
-  it('contains an image', function() {
 
-  })
 
 
 });
