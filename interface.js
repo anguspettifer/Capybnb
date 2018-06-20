1d1ea1d4acc7ad21d27d3b497f086a1cf4970a88
CharlottePayne <35195024+CLTPayne@users.noreply.github.com>
2018-06-20 14:35:19 +0100

    Refactor add title into constructor function

--- interface.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function(){
-
-  $('#addtitle').click(function(){
-    event.preventdefault();
-    var title = $('#title').val();
-    $('#display-title').text(title);
-  })
-});
--- src/interface.js
+++ src/interface.js
@@ -1,11 +1,20 @@
 $(document).ready( function () {
   var addContent = new AddContent();
 
-  $('#add-property-description').submit( function(event) {
+  $('#addtitle').click(function(){
+    event.preventdefault();
+    var title = $('#title').val();
+    $('#display-title').text(title);
+  })
+
+  $('#add-property').submit( function(event) {
     event.preventDefault();
+    var title = $('#title').val();
     var description = $('#description').val();
+    addContent.addTitle(title)
     addContent.addDescription(description);
-    $('#property-description').text(addContent.getDescription());
+    $('#display-title').text(addContent.getTitle());
+    $('#display-description').text(addContent.getDescription());
   });
 
 });
